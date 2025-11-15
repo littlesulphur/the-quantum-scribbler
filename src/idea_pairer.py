925cf3fd0a92c05cfcf16d47cfcff98abd973646
suLphuR <sarthakdashmohapatra@outlook.com>
2025-11-15 16:46:38 -0500

    Adding __init__, config and idea_pairer python files

---
+++ src/idea_pairer.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import random
+import re
+import spacy
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+
+
+nlp = spacy.load("en_core_web_sm")
+STOPWORDS = set(stopwords.words("english"))
+
+
+def clean_text(text):
+    """ Simple Cleaning for headline """
+    if not isinstance(text, str):
+        return ""
+    text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+    text = text.lower()
+    return text
+
+
+def extract_keywords(text, top_n=5):
+    """ Extract noun chunks or keywords using Spacy """
+    doc = nlp(text)
+    keywords = [chunk.text for chunk in doc.noun_chunks if chunk.text.lower() not in STOPWORDS]
+    return keywords[:top_n]
+
+
+def load_news(filepath):
+    """ Load the news """
+    df = pd.read_csv(filepath)
+    df["cleaned"] = df["title"].apply(clean_text)
+    return df
+
+
+def compute_distance_matrix(texts):
+    """ Compute pair-wise cosine similarity (lower = more novel pairing) """
+    vectorizer = TfidVectorizer(stop_words='english')
+    tfidf = vectorizer.fit_transform(texts)
+    sim_matrix = cosine_similarity(tfidf)
+    return 1 - sim_matrix   #distance = inverse of similarity
+
+
+def generate_random_pairs(df, n_pairs=5):
+    """ Generate pairs of distant topics """
+    
